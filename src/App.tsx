3c627c80074a92ec9f3dc1a8d838a81197e150a6
eduadg <eduadg@hotmail.com>
2023-09-04 16:41:12 -0300

    Refatorado a criação do root para prevenir repetições.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { createRoot } from 'react-dom';
 import Logo from './components/Header/Logo';
 import FormularioLogin from './components/Header/FormularioLogin';
 import LinksAuxiliares from './components/Header/LinksAuxiliares';
@@ -15,13 +14,7 @@ const TelaLogin: React.FC = () => (
   </div>
 );
 
-const root = document.getElementById('root');
-const appRoot = createRoot(root);
-
-appRoot.render(<TelaLogin />);
-
 export default TelaLogin;
-
 
 
 // Utilizando a nova API createRoot do React 18
