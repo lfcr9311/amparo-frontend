c81cc8b693768a4fa7deba568da2ebf959fcd222
Luis Felipe Copetti Rossi <rossi.luis@edu.pucrs.br>
2023-09-08 00:34:48 -0300

    fazendo componente de selecao de Estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import React, { useState } from "react";
 import SelectComponent from "./components/Select";
+import { SelectChangeEvent } from "@mui/material";
 
 function App() {
+  const [selectedState, setSelectedState] = useState(""); 
+
+  const handleStateChange = (event: SelectChangeEvent) => {
+    setSelectedState(event.target.value as string); 
+    console.log(event.target.value);
+  };
+
   return (
     <div className="App">
       <SelectComponent 
-        label="UF"
-        value=""
-        onChange={() => console.log("Ola")}
+        label="Estado"
+        value={selectedState}
+        onChange={handleStateChange}
       />
     </div>
   );
