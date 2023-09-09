5c4cdb3985ac360c55eaf59bfa4ee0e17e125258
Luis Felipe Copetti Rossi <rossi.luis@edu.pucrs.br>
2023-09-09 19:35:25 -0300

    Iniciando Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,38 +5,56 @@ import ChatIcon from '@mui/icons-material/Chat';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 const containerStyle: React.CSSProperties = {
-  backgroundColor: '#FFFFFF',
-  width: "375px",
+  backgroundColor: '',
+  width: "auto",
   height: "109px",
+  gap: "75px",
+  display: "flex",
+  justifyContent: "center",
 };
 
 const iconStyle: React.CSSProperties = {
-    height: "27px",
-    width: "27px",
-    marginLeft: "12px",
-    alignItems: "center",
-    justifyContent: "center",
-    display: "flex",
-    padding: '35px',
-}
+  alignItems: "center",
+  justifyContent: "center",
+  display: "flex",
+  width: "27px",  
+  height: "27px",
+};
+
+const textStyle: React.CSSProperties = {
+  textAlign: "center",
+  marginTop: "5px",
+  fontFamily: "Poppins, sans-serif",
+  fontSize: "12px",
+  justifyContent: "center",
+};
 
 interface FooterProps {
-    onClickHome: () => void;
-    onClickChat: () => void;
-    onClickPerfil: () => void;
+  onClickHome: () => void;
+  onClickChat: () => void;
+  onClickPerfil: () => void;
 }
 
 export const Footer: React.FC<FooterProps> = ({ onClickHome, onClickChat, onClickPerfil }) => { 
   return (
-    <footer style={containerStyle}>    
-      <IconButton onClick={onClickHome} >
-        <HomeIcon style={iconStyle}/>
+    <footer style={containerStyle}> 
+      <IconButton onClick={onClickHome}>
+        <div>
+          <HomeIcon style={iconStyle}/>
+          <div style={textStyle}>Home</div>
+        </div>
       </IconButton>
       <IconButton onClick={onClickChat}>
-        <ChatIcon style={iconStyle}/>
+        <div>
+          <ChatIcon style={iconStyle}/>
+          <div style={textStyle}>Chat</div>
+        </div>
       </IconButton>
       <IconButton onClick={onClickPerfil}>        
-        <AccountBoxIcon style={iconStyle}/>
+        <div>
+          <AccountBoxIcon style={iconStyle}/>
+          <div style={textStyle}>Perfil</div>
+        </div>
       </IconButton>
     </footer>
   );
