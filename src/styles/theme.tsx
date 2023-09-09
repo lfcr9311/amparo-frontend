9175bde0d8841059dfb88b07025267f27377e08c
Clara D'Ávila <claracunhadavila@gmail.com>
2023-09-09 17:59:50 -0300

    adicionando styles do app

---
+++ src/styles/theme.tsx
@@ -0,0 +1,28 @@
+const theme = {
+    colors: {
+        background: "#FBF8F8",
+        text: "#4D4C4C",
+        textDescription: "#696969",
+        primary: "#E10E17",
+        iconSelected: "#00B563",//
+        border: "#EBECF3",//
+        outline: "#FF9001",//
+        disabled: "#C9CAD4",//
+        filterClicked: "#E3E4EB",//
+        textDisabled: "#9599A6", //
+        graph: [
+            "#BD1E08",
+            "#E10E17",
+            "#E76553",
+            "#FBB8B1",
+            "#FF3825",
+            "#E7C298",
+            "#170401",
+            "#FBF8F8",
+            "696969",
+            "4D4C4C",
+        ],
+    },
+};
+
+export default theme;
