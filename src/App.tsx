7d4be7d097720532ee105af4e7fb770a3fbb4ab1
João Silva <jpsouzadasilva@gmail.com>
2023-09-09 20:00:10 -0300

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import Login from './pages/Login';
-import Footer from './components/Footer';
-
-function onClick(){
-  
-}
 
 function App() {
   return (
-    <div  className="App">
-      <Footer />
+    <div className="App">
+      <Login />
     </div>
   );
 }
