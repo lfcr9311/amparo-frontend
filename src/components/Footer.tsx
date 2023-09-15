291dbd9dbffc540eeec2a607d7d38cca2ffd7d01
Lucas Quaresma <delmar.quaresma@edu.pucrs.br>
2023-09-15 01:01:14 -0300

    Retirando as props do footer. Pages no component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import IconButton from "@mui/material/IconButton";
-import HomeIcon from '@mui/icons-material/Home';
-import ChatIcon from '@mui/icons-material/Chat';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import HomeIcon from '../assets/Home.svg';
+import ChatIcon from '../assets/Chat.svg';
+import ProfileIcon from '../assets/Profile.svg';
+import Icon from "@mui/material/Icon";
+
 
 const containerStyle: React.CSSProperties = {
   width: "auto",
@@ -11,8 +14,8 @@ const containerStyle: React.CSSProperties = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  // marginTop: 'auto',
-  position: 'fixed',
+  //marginTop: "50px",
+  position: 'absolute',
   bottom: 0, // Position at the bottom of the nearest positioned ancestor
   left: 0,   // Position at the left edge of the nearest positioned ancestor
   right: 0,  // Positi
@@ -35,34 +38,49 @@ const textStyle: React.CSSProperties = {
   alignItems: "center",
 };
 
-interface FooterProps {
-  onClickHome: () => void;
-  onClickChat: () => void;
-  onClickPerfil: () => void;
-}
+// interface FooterProps {
+//   onClickHome: () => void;
+//   onClickChat: () => void;
+//   onClickPerfil: () => void;
+// }
 
-export const Footer: React.FC<FooterProps> = ({ onClickHome, onClickChat, onClickPerfil }) => {
+export const Footer: React.FC = ({}) => {
+  const navigate = useNavigate();
+
+  const handleCLickPerfil = () => {
+    navigate('/perfil/paciente')
+  }
+
+  const handleClickHome = () => {
+    navigate("/home/paciente")
+  }
   return (
     <footer style={containerStyle}>
-      <IconButton onClick={onClickHome}
+      <IconButton onClick={handleClickHome} //onClickHome
         sx={{ color: "#000000" }}>
         <div>
-          <HomeIcon style={iconStyle} />
+          <Icon style={iconStyle}>
+            <img src={HomeIcon} />
+          </Icon>
           <div style={textStyle}>Home</div>
         </div>
       </IconButton>
-      <IconButton onClick={onClickChat}
+      <IconButton onClick={handleClickHome} //onClickChat
         sx={{ color: "#000000" }}>
         <div>
-          <ChatIcon style={iconStyle} />
+          <Icon style={iconStyle}>
+            <img src={ChatIcon} />
+          </Icon>
           <div style={textStyle}>Chat
           </div>
         </div>
       </IconButton>
-      <IconButton onClick={onClickPerfil}
+      <IconButton onClick={handleCLickPerfil} //onClickPerfil
         sx={{ color: "#000000" }}>
         <div>
-          <AccountBoxIcon style={iconStyle} />
+          <Icon style={iconStyle}>
+            <img src={ProfileIcon} />
+          </Icon>
           <div style={textStyle}>Perfil</div>
         </div>
       </IconButton>
