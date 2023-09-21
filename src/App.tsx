d80ad457ade9600bbd995dfedf7f2b140b47a703
Lucas S. Wolschick <lucas.wolschick@edu.pucrs.br>
2023-09-21 20:59:27 -0300

    add componente do botao salmao

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import ExamesPendentesVazio from './pages/ExamesPendentes/ExamesPendentesVazio';
 import { CadastroPaciente } from './pages/CadastroPaciente/CadastroPaciente';
 import { CadastroMedico } from './pages/CadastroMedico/CadastroMedico';
 import { Identificacao } from './pages/Identificacao/Identificacao';
+import { ButtonSalmon } from './components/ButtonSalmon/ButtonSalmon';
+
+import setaConfirmaIcon from './assets/setaConfirmaIcon.png';
+
 // Quando os componentes CadastroPaciente e EsqueceuSenha estiverem disponíveis, descomentar os imports abaixo.
 // import EsqueceuSenha from './pages/EsqueceuSenha/EsqueceuSenha';
 
@@ -18,6 +22,7 @@ function App() {
         <Routes>
           {/*<Route path="/" element={<Login />} />*/}
           {/* Descomente as linhas abaixo quando os componentes estiverem disponíveis */}
+          <Route path="/button-salmon" element={<ButtonSalmon label='Realizado' onClick={() => console.log('buttonSalmonClicado')} icon={<img src={setaConfirmaIcon} />} />} />
           <Route path="/home/paciente" element={<HomePaciente />} />
           <Route path="/cadastro/paciente" element={<CadastroPaciente />} />
           <Route path="/cadastro/medico" element={<CadastroMedico />} />
---
+++ src/components/ButtonSalmon/ButtonSalmon.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+
+interface CustomButtonSalmonProps {
+    label: string;
+
+    icon?: React.ReactElement;  //Testar
+
+    onClick: () => void;
+
+
+
+
+}
+interface IconButtonProps {
+    icon: React.ReactElement;
+    style?: React.CSSProperties;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({ icon, style }) => {
+    const iconStyle: React.CSSProperties = {
+
+        maxWidth: '26px',
+        maxHeight: '26px',
+        margin: '0 8px 0 0',
+        ...style,
+    };
+    return <span style={iconStyle}>{icon}</span>;
+};
+
+
+
+
+export const ButtonSalmon: React.FC<CustomButtonSalmonProps> = ({ label, icon, onClick, }) => {
+
+
+    const buttonStyle: React.CSSProperties = {
+        backgroundColor: "#E76553",
+        display: 'flex',
+        margin: '8px',
+        color: 'white',
+        fontFamily: 'Poppins',
+        fontSize: '19px',
+        fontWeight: '500',
+        borderRadius: "10px",
+        height: "53px",
+        //        width: "152px",
+
+
+    };
+
+
+
+
+    return (
+        <div className="ButtonSalmon">
+            <Button variant="contained"
+                onClick={onClick}
+                style={buttonStyle}>
+                {icon && <IconButton icon={icon} />} {/* Usando o IconButton */}
+                <span>{label}</span>
+
+            </Button>
+        </div>
+    );
+} 
