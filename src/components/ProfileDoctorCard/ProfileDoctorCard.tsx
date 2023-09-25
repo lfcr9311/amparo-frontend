68d5396dc49c6132e84ef0cf1d0f91f440d7c62a
João Silva <jpsouzadasilva@gmail.com>
2023-09-25 18:09:04 -0300

    Finalizando CardDoctor

--- src/components/ProfileDoctorCard/ProfileDoctorCard.tsx
+++ src/components/ProfileDoctorCard/ProfileDoctorCard.tsx
@@ -1,16 +1,21 @@
 import './ProfileDoctorCard.css';
 import EditIcon from '../../assets/EditIcon.svg';
 import IconProfile from '../../assets/IconProfile.svg';
+import EmailIcon from '../../assets/EmailIcon.svg';
+import PhoneIcon from '../../assets/PhoneIcon.svg';
 import { Switch } from '@mui/material';
+import { useState } from 'react';
 
 interface ProfileDoctorCardProps {
   name: string;
   specialty: string;
   agreements: string;
   crm: number;
+  profilePicture?: string;
   email: string;
   phone: string;
   edit: () => void;
+  alterarSenha: () => void;
 }
 
 export default function ProfileDoctorCard({
@@ -21,7 +26,16 @@ export default function ProfileDoctorCard({
   email,
   phone,
   edit,
+  alterarSenha,
+  profilePicture,
 }: ProfileDoctorCardProps) {
+  const [notifications, setNotifications] = useState(false);
+
+  const changeNotifications = (notifications: boolean) => {
+    if (notifications) console.log('Notificações desabilitadas');
+    else console.log('Notificações habilitadas');
+  };
+
   return (
     <div className="card-container">
       <button onClick={edit} className="edit-button">
@@ -41,9 +55,37 @@ export default function ProfileDoctorCard({
         </div>
         <div className="notifications-container">
           <p className="notifications">Habilitar Notificacoes</p>
-          <Switch sx={{ marginLeft: '80px' }} />
+          <Switch
+            sx={{ marginLeft: '80px', marginTop: '5px' }}
+            checked={notifications}
+            onChange={() => {
+              setNotifications(!notifications);
+              changeNotifications(notifications);
+            }}
+          />
         </div>
       </div>
+      <div className="aux-box">
+        <span className="itens">
+          <img
+            src={EmailIcon}
+            className="aux-box-icon email-icon"
+            alt="Email"
+          />
+          {email}
+        </span>
+        <span className="itens">
+          <img
+            src={PhoneIcon}
+            className="aux-box-icon phone-icon"
+            alt="Phone"
+          />
+          {phone}
+        </span>
+      </div>
+      <button className="alterar-senha-button" onClick={alterarSenha}>
+        Alterar senha
+      </button>
     </div>
   );
 }
