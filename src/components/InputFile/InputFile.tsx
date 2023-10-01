645e7bf4800c830d8aa2a7037e5a21d2d3824797
João Silva <jpsouzadasilva@gmail.com>
2023-10-01 20:27:22 -0300

    ATT

--- src/components/InputFile/InputFile.tsx
+++ src/components/InputFile/InputFile.tsx
@@ -5,15 +5,22 @@ import './InputFile.css';
 
 interface FileInputProps {
   type: 'image' | 'pdf';
+  onChange: (file: File | null) => void;
 }
 
-export default function InputFile({ type }: FileInputProps) {
+export default function InputFile({ type, onChange }: FileInputProps) {
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0] || null;
+    onChange(file);
+  };
+
   return (
     <label className="file-input-label">
       <input
         type="file"
         accept={type === 'image' ? 'image/*' : 'application/pdf'}
         className="file-input"
+        onChange={handleFileChange}
       />
       <img src={type === 'image' ? AddImageIcon : AddFileIcon} />
       {type === 'image' ? 'Adicionar Imagem/Foto' : 'Adicionar Arquivo PDF'}
