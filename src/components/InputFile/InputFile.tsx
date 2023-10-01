3eff6e29c9b4340e3b7e9aff91782f64c2f27d7b
João Silva <jpsouzadasilva@gmail.com>
2023-10-01 01:42:21 -0300

    commit inicial

---
+++ src/components/InputFile/InputFile.tsx
@@ -0,0 +1,10 @@
+import AddImageIcon from '../../assets/AddImageIcon.svg';
+import AddFileIcon from '../../assets/AddFileIcon.svg';
+import './InputFile.css';
+
+interface InputFileProps {
+  type: 'image' | 'file';
+}
+export default function InputFile({ type }: InputFileProps) {
+  return <></>;
+}
