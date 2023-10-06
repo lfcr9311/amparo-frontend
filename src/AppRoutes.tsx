3d9e59a7e76dba41f53676b4ee02db0821a36a79
Lucas Quaresma <delmar.quaresma@edu.pucrs.br>
2023-10-06 16:38:41 -0300

    Adicionando rotas faltantes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -11,6 +11,9 @@ import { CadastroMedico } from './pages/CadastroMedico/CadastroMedico';
 import { Identificacao } from './pages/Identificacao/Identificacao';
 import HomeMedico from './pages/HomeMedico/HomeMedico';
 import VisualizacaoPerfilMedico from './pages/VisualizarPerfilMedico/VisualizarPerfilMedico';
+import MenuMedicamentos from './pages/MenuMedicamentos/MenuMedicamentos';
+import ListaMedicamentos from './pages/ListaMedicamentos/ListaMedicamentos';
+import ExamesPendentes from './pages/ExamesPendentes/ExamesPendentes';
 import Temporaria from './pages/temporaria';
 
 export default function AppRoutes() {
@@ -30,8 +33,11 @@ export default function AppRoutes() {
             path={ROUTES.EXAMES_PENDENTES_VAZIO()}
             element={<ExamesPendentesVazio />}
           />
-          <Route path={ROUTES.LOGIN()} element={<Login />} 
+          <Route
+            path={ROUTES.EXAMES_REALIZADOS_VAZIO()}
+            element={<ExamesRealizadosVazio />}
           />
+          <Route path={ROUTES.LOGIN()} element={<Login />} />
           <Route
             path={ROUTES.PERFIL_PACIENTE()}
             element={<VisualizacaoPerfilPaciente />}
@@ -39,7 +45,17 @@ export default function AppRoutes() {
           <Route
             path={ROUTES.PERFIL_MEDICO()}
             element={<VisualizacaoPerfilMedico />}
-          /> 
+          />
+          <Route
+            path={ROUTES.MENU_MEDICAMENTOS()}
+            element={<MenuMedicamentos />}
+          />
+          <Route
+            path={ROUTES.LISTA_MEDICAMENTOS()}
+            element={<ListaMedicamentos />}
+          />
+
+          <Route path={ROUTES.LISTA_EXAMES()} element={<ExamesPendentes />} />
         </Routes>
       </div>
     </Router>
