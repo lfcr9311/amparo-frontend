608ac96cf8f59de4b3ec052acefab648b50ce2d4
Pablo Montiel <pablopmontiel98@gmail.com>
2023-10-11 00:23:20 -0300

    Modificações Prettier

--- src/components/ButtonSalmon/ButtonSalmon.tsx
+++ src/components/ButtonSalmon/ButtonSalmon.tsx
@@ -5,61 +5,53 @@ import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 import Box from '@mui/material/Box';
 
 interface CustomButtonSalmonProps {
-    label: string;
-    icon?:boolean;  //Testar
-    onClick: () => void;
+  label: string;
+  icon?: boolean; //Testar
+  onClick: () => void;
 }
 
-export const ButtonSalmon: React.FC<CustomButtonSalmonProps> = ({ label, icon, onClick, },props: SvgIconProps) => {
-
-    const buttonStyle: React.CSSProperties = {
-        backgroundColor: "#E76553",
-        display: 'flex',
-        margin: '8px',
-        color: 'white',
-        fontFamily: 'Poppins',
-        fontSize: '19px',
-        fontWeight: '500',
-        borderRadius: "10px",
-        height: "53px",
-        width: "164px",
-    };
-    if (!icon){
-        return (
-            <Button
-                style={buttonStyle}            
-                onClick={onClick}
-                variant='contained'
-            >
-                {label}
-            </Button>
-        );
-    }
-    else{
-        return(
-            <Button
-
-            
-            sx={{
-                '& > :not(style)': {
-                  m: 1,
-                  marginTop:2.5,
-                },
-              }}
-
-
-            style={buttonStyle}            
-            onClick={onClick}
-            variant='contained'
-            
-            startIcon = {<Box><CheckCircleOutlineIcon sx={{fontSize: 30}}/> </Box>}
-            
-                  
-            >
-                {label}
-            </Button>
-            
-        );
-    }
-
-} 
+export const ButtonSalmon: React.FC<CustomButtonSalmonProps> = (
+  { label, icon, onClick },
+  props: SvgIconProps
+) => {
+  const buttonStyle: React.CSSProperties = {
+    backgroundColor: '#E76553',
+    display: 'flex',
+    margin: '8px',
+    color: 'white',
+    fontFamily: 'Poppins',
+    fontSize: '19px',
+    fontWeight: '500',
+    borderRadius: '10px',
+    height: '53px',
+    width: '164px',
+  };
+  if (!icon) {
+    return (
+      <Button style={buttonStyle} onClick={onClick} variant="contained">
+        {label}
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        sx={{
+          '& > :not(style)': {
+            m: 1,
+            marginTop: 2.5,
+          },
+        }}
+        style={buttonStyle}
+        onClick={onClick}
+        variant="contained"
+        startIcon={
+          <Box>
+            <CheckCircleOutlineIcon sx={{ fontSize: 30 }} />{' '}
+          </Box>
+        }
+      >
+        {label}
+      </Button>
+    );
+  }
+};
