e9d4e72bd7b33a44cf7e6b776263c6d057a25ec7
Lucas S. Wolschick <lucas.wolschick@edu.pucrs.br>
2023-10-23 12:49:32 -0300

    Criacao do comp

---
+++ src/components/ModalPerfilPaciente/Modal.tsx
@@ -0,0 +1,33 @@
+import './Modal.css';
+import Close from '../../assets/Close.svg';
+import React from 'react';
+
+interface ModalProps {
+  isOpen: boolean;
+  title?: string;
+  isClose: any;
+  children?: React.ReactNode;
+}
+
+export default function ModalPerfilPaciente({
+  isOpen,
+  title,
+  isClose,
+  children,
+}: ModalProps) {
+  if (isOpen) {
+    return (
+      <div className="background-container">
+        <div className="modal-container">
+          <div className="header-modal-container">
+            <p className="title-modal">{title}</p>
+            <button className="button-close" onClick={isClose}>
+              <img src={Close} />
+            </button>
+          </div>
+          <div className="body-modal-container">{children}</div>
+        </div>
+      </div>
+    );
+  }
+}
