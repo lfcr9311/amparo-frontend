60c2fcda6e846a342ed4737d15e8072608e55fc0
Pablo Montiel <pablopmontiel98@gmail.com>
2023-10-25 17:57:34 -0300

    Hotfix da seleção dos horários

--- src/components/Modal/Components/SelectTime/SelectTime.tsx
+++ src/components/Modal/Components/SelectTime/SelectTime.tsx
@@ -1,12 +1,23 @@
+import { ChangeEvent, useState } from 'react';
 import './SelectTime.css';
 
 export default function SelectTime() {
+    const [selectedTime, setSelectedTime] = useState('00:00');
+
+    const handleTimeChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSelectedTime(event.target.value);
+    }
 
     return (
         <div className='componente-time'>
             <label className='horario'>
-                Horário de Adminitstração:
-                <input className='hora-minuto' type='time' required/>
+                Horário de Administração:
+                <input
+                    className='hora-minuto'
+                    type='time'
+                    value={selectedTime}
+                    onChange={handleTimeChange}
+                />
             </label>
         </div>
     )
