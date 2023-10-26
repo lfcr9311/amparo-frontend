39410c320e088c34b03ebf3b10a4a4518ffb5a0e
Pablo Montiel <pablopmontiel98@gmail.com>
2023-10-26 17:18:56 -0300

    hotfix MR

---
+++ src/pages/AddMedicamentoAgenda/AddMedicamentoAgenda.tsx
@@ -0,0 +1,47 @@
+import CustomButton from '../../components/Button/Button';
+import Footer from '../../components/Footer/Footer';
+import HeaderHome from '../../components/HeaderHome/HeaderHome';
+import SelectMedicamento from '../../components/Modal/Components/SelectMedicamento/SelectMedicamento';
+import SelectTime from '../../components/Modal/Components/SelectTime/SelectTime';
+import Modal from '../../components/Modal/Modal';
+import './AddMedicamentoAgenda.css';
+import { useState } from 'react';
+
+export default function AddMedicamentoAgenda() {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [tempTime, setTempTime] = useState();
+
+    const handleValues = () => {
+        setTempTime(tempTime);
+    }
+
+    return (
+        <>
+            <HeaderHome title='Agenda' type='headerPage' />
+            <Modal isOpen={true} isClose={() => setModalIsOpen(!modalIsOpen)} title='Medicamento'>
+                <div className='medication-info'>
+                    <SelectMedicamento
+                        value=''
+                        onChange={() => {}}
+                    />
+                </div>
+                <div className='continuo'>
+                        <input type='checkbox'/>
+                        <label>Uso contínuo</label>
+                </div>
+                <div className='administration'>
+                    <SelectTime/>
+                </div>
+                <div className='botao'>
+                <CustomButton
+                        variant='contained'
+                        label='Salvar'
+                        onClick={handleValues}
+                    />
+                </div>
+            </Modal>
+            <Footer user='patient'/>
+        </>
+    )
+}
