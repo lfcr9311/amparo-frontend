9e7dbf65c930d3d3aba829d2b83a2f95e683e5fb
Lucas S. Wolschick <lucas.wolschick@edu.pucrs.br>
2023-10-26 11:23:14 -0300

    Finalizacao Pagina

--- src/pages/ListaDeInteracaoDoMedicamento/ListaDeInteracaoDomedicamento.tsx
+++ src/pages/ListaDeInteracaoDoMedicamento/ListaDeInteracaoDomedicamento.tsx
@@ -5,10 +5,14 @@ import HeaderHome from '../../components/HeaderHome/HeaderHome';
 import Footer from '../../components/Footer/Footer';
 import './ListaDeInteracaoDoMedicamento.css';
 
+interface ListaDeInteracaoDoMedicamentoProps {
+  nomeDoMedicamentoQuePossuiInteracao?: string;
+  listaRecebidaDeInteracoesDoMedicamento?: { name: string; status: string }[];
+}
 
-
-const US21ListaDeInteracaoMedicamentos = () => {
+const US21ListaDeInteracaoMedicamentos = ({ nomeDoMedicamentoQuePossuiInteracao = "Medicamento" }: ListaDeInteracaoDoMedicamentoProps) => {
   const [filtroStatus, setFiltroStatus] = useState('semFiltroDeStatus');
+
   const [filtroText, setFiltroText] = useState('');
   const [listaDeMedicamentosCompleta] = useState([
     { name: ' Alfa', status: 'bom' },
@@ -17,6 +21,12 @@ const US21ListaDeInteracaoMedicamentos = () => {
     { name: ' dionisio', status: 'bom' },
     { name: ' Blue', status: 'medio' },
     { name: ' Ccaraca', status: 'ruim' },
+    { name: ' Aasd', status: 'bom' },
+    { name: ' Bdas', status: 'medio' },
+    { name: ' Csdasd', status: 'ruim' },
+    { name: ' Aasd', status: 'bom' },
+    { name: ' Bfa', status: 'medio' },
+    { name: ' Cfas', status: 'ruim' },
     { name: ' Aasd', status: 'bom' },
     { name: ' Bdas', status: 'medio' },
     { name: ' Csdasd', status: 'ruim' },
@@ -32,27 +42,24 @@ const US21ListaDeInteracaoMedicamentos = () => {
   const handleFiltroTextChange = (text: string) => {
     setFiltroText(text);
   };
+
+
   return (
     <>
       <HeaderHome title="Medicamentos" type="headerPage" />
       <div className='body-container'>
-        <div className='title-body'>Paracetamol</div>
+        <div className='title-body' title={nomeDoMedicamentoQuePossuiInteracao}>{nomeDoMedicamentoQuePossuiInteracao}</div>
 
         <div className='body-filtro'>
           <FiltroBuscaMedicamentos onStatusChange={handleFiltroStatusChange} onNameChange={handleFiltroTextChange} status={filtroStatus} />
         </div>
         <div className='body-lista'>
-
           <ListaInteracoes items={listaDeMedicamentosCompleta.filter(item => (item.status === filtroStatus || filtroStatus === 'semFiltroDeStatus') &&
             item.name.toLowerCase().includes(filtroText.toLowerCase()))} />
         </div>
 
       </div>
-
-
-
       <Footer user="patient" />
-
     </>
   );
 };
