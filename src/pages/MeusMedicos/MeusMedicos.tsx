d121a3e0dc50ce44c26b493bffde2bd0953fde01
João Coelho <joaoantonioscoelho@gmail.com>
2023-10-30 20:02:27 -0300

    refactor MeusMedicos + adiciona spinner de loading

--- src/pages/MeusMedicos/MeusMedicos.tsx
+++ src/pages/MeusMedicos/MeusMedicos.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import HeaderHome from '../../components/HeaderHome/HeaderHome';
-import './MeusMedicosStyle.css';
+import './MeusMedicosStyle.css'; // Importe o arquivo CSS
 import Footer from '../../components/Footer/Footer';
 import Modal from '../../components/Modal/Modal';
 import ModalDetalhesMedico from '../../components/ModalDetalhesMedico/ModalDetalhesMedico';
 import Solicitacao from '../../components/Modal/Components/Solicitacao/SolicitacaoModal';
+import CardUsuario from '../../components/CardUsuario/CardUsuario';
+import { CircularProgress } from '@mui/material';
 import { fetchMeusMedicos } from '../../utils/apiService.tsx';
-import CardUsuario from '../../components/CardUsuario/CardUsuario.tsx';
 
 interface Medico {
   id: number;
@@ -26,32 +27,36 @@ const MeusMedicos: React.FC = () => {
   const [originalMedicos, setOriginalMedicos] = useState<Medico[]>([]);
   const [medico, setMedico] = useState<Medico>();
   const [searchText, setSearchText] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetchMeusMedicos().then((doctors) => {
-      setOriginalMedicos(doctors);
-      setMedicos(doctors);
-      console.log(doctors);
-    }).catch((err) => {
-      console.error(err);
-    });
+    const fetchData = async () => {
+      try {
+        const doctors = await fetchMeusMedicos();
+        setOriginalMedicos(doctors);
+        setMedicos(doctors);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  function handleCardClick(medico: Medico) {
+  const handleCardClick = (selectedMedico: Medico) => {
     setisModalDetalhesOpen(!isModalDetalhesOpen);
-    setMedico(medico);
-  }
+    setMedico(selectedMedico);
+  };
 
-  function handleAddButtonClick() {
+  const handleAddButtonClick = () => {
     setisModalSolicitacaoOpen(!isModalSolicitacaoOpen);
-  }
+  };
 
   useEffect(() => {
     const normalizeText = (text: string) =>
-      text
-        .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '')
-        .toLowerCase();
+      text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
 
     const normalizedSearchText = normalizeText(searchText);
 
@@ -65,61 +70,63 @@ const MeusMedicos: React.FC = () => {
   return (
     <>
       <HeaderHome title='Meus Médicos' type='headerPage' />
-      <div className='search-container'>
-        {/* TODO: temporário, componente de busca está em desenvolvimento */}
-        <input
-          type='text'
-          placeholder='Buscar...'
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='36'
-          height='36'
-          viewBox='0 0 36 36'
-          fill='none'
-          onClick={() => handleAddButtonClick()}
-        >
-          <path
-            d='M18 0C8.064 0 0 8.064 0 18C0 27.936 8.064 36 18 36C27.936 36 36 27.936 36 18C36 8.064 27.936 0 18 0ZM27 19.8H19.8V27H16.2V19.8H9V16.2H16.2V9H19.8V16.2H27V19.8Z'
-            fill='#E10E17'
-          />
-        </svg>
-      </div>
-      <div className='my-doctors-container'>
-        {medicos.length > 0 ? (
-          medicos.map((medico) => (
-            <CardUsuario
-              key={medico.id}
-              name={medico.name}
-              profilePicture={medico.profilePicture}
-              onClick={() => handleCardClick(medico)}
+      {isLoading && <CircularProgress color="error" />}
+      {!isLoading && (
+        <>
+          <div className='search-container'>
+            <input
+              className='search-input' // Adicione a classe CSS para o input
+              type='text'
+              placeholder='Buscar...'
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
             />
-          ))
-        ) : (
-          <div className='no-doctors'>
-            <h3>Você não possui médicos cadastrados.</h3>
-            <p>
-              Clique no botão de adicionar para solicitar um novo médico.
-            </p>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='36'
+              height='36'
+              viewBox='0 0 36 36'
+              fill='none'
+              onClick={handleAddButtonClick}
+            >
+              <path
+                d='M18 0C8.064 0 0 8.064 0 18C0 27.936 8.064 36 18 36C27.936 36 36 27.936 36 18C36 8.064 27.936 0 18 0ZM27 19.8H19.8V27H16.2V19.8H9V16.2H16.2V9H19.8V16.2H27V19.8Z'
+                fill='#E10E17'
+              />
+            </svg>
           </div>
-        )}
-      </div>
+          <div className='my-doctors-container'>
+            {medicos.length > 0 ? (
+              medicos.map((medico) => (
+                <CardUsuario
+                  key={medico.id}
+                  name={medico.name}
+                  profilePicture={medico.profilePicture}
+                  onClick={() => handleCardClick(medico)}
+                />
+              ))
+            ) : (
+              <div className='no-doctors'>
+                <h3>Não foram encontrados médicos.</h3>
+                <p>Clique no botão de adicionar para solicitar um novo médico.</p>
+              </div>
+            )}
+          </div>
+        </>
+      )}
 
       <Modal
         isOpen={isModalDetalhesOpen}
         title=''
         isClose={() => setisModalDetalhesOpen(!isModalDetalhesOpen)}
-        children={
-          <ModalDetalhesMedico
-            medico={medico}
-            isModalOpen={isModalDetalhesOpen}
-            setIsModalOpen={setisModalDetalhesOpen}
-            setMedico={setMedico}
-          />
-        }
-      />
+      >
+        <ModalDetalhesMedico
+          medico={medico}
+          isModalOpen={isModalDetalhesOpen}
+          setIsModalOpen={setisModalDetalhesOpen}
+          setMedico={setMedico}
+        />
+      </Modal>
       <Solicitacao
         isModalOpen={isModalSolicitacaoOpen}
         setIsModalOpen={setisModalSolicitacaoOpen}
