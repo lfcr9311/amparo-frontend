61bc915890a20621d19ba503e3854952eb570395
Lucas S. Wolschick <lucas.wolschick@edu.pucrs.br>
2023-11-02 01:41:35 -0300

    Add button e style

--- src/pages/EscreveTextoForum/EscreveTextoForum.tsx
+++ src/pages/EscreveTextoForum/EscreveTextoForum.tsx
@@ -1,28 +1,55 @@
-import React from "react";
+
 import { MultilineTextFields } from "../../components/TextFieldForum/ComponentTextFieldForum";
 import HeaderHome from "../../components/HeaderHome/HeaderHome";
 import Footer from "../../components/Footer/Footer";
+import CustomButton from '../../components/Button/Button';
+
+import "./EscreveTextoForum.css";
 
 
 export default function EscreveTextoForumProps() {
     return (
         <>
-            <HeaderHome title="Informações" type="headerPage" />        <div className="texto-informacoes-box">
-                <div className="texto-informacoes-titulo">
+            <HeaderHome title="Informações" type="headerPage" />
+            <div className="escreve-informacoes-box">
+                <div >
                     <MultilineTextFields tituloDoTextField={"Título"}
                         quantidadeLinhas={2}
-                        mensagemQuadroEmBranco={"Insira o título"}
+                        mensagemQuadroEmBranco={"Insira o título..."}
+                    />
+                </div>
+                <br />
+
+                <div >
+                    <MultilineTextFields tituloDoTextField={"Subtítulo"}
+                        quantidadeLinhas={2}
+                        mensagemQuadroEmBranco={"Insira a prévia..."}
+                    />
+                </div>
+                <br />
+
+                <div>
+                    <MultilineTextFields tituloDoTextField={"Texto Informativo"}
+                        quantidadeLinhas={10}
+                        mensagemQuadroEmBranco={"Insira o texto..."}
+                    />
+                </div>
+                <br />
+                <div >
+                    <MultilineTextFields tituloDoTextField={"Links"}
+                        quantidadeLinhas={3}
+                        mensagemQuadroEmBranco={"Insira as referências..."}
+                    />
+                </div>
+                <div className="escreve-button-salvar">
+                    <CustomButton
+                        variant="contained"
+                        label="Salvar"
+                    // onClick={() => console.log('salvar')}        
+
                     />
                 </div>
 
-                <div className="texto-informacoes-previa"></div>
-                <br />
-                <div className="texto-informacoes-data-autor"></div>
-                <br />
-                <div className="texto-informacoes-texto"></div>
-                <br />
-                <br />
-                <div className="texto-informacoes-referencias"></div>
             </div>
             <Footer user="patient" />
         </>
