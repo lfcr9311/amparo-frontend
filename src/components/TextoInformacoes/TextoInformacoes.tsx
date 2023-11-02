8668ab96cf978138323f624c496851ae35ac8301
Lucas S. Wolschick <lucas.wolschick@edu.pucrs.br>
2023-11-02 20:16:38 -0300

    Finalizacao do componente de publicacao do forum

--- src/components/TextoInformacoes/TextoInformacoes.tsx
+++ src/components/TextoInformacoes/TextoInformacoes.tsx
@@ -7,13 +7,13 @@ interface TextoInformacoesProps {
     autor: string;
     data: string;
     texto: string;
-    referencias: string;
+    referencias: string[];
 }
 
 const ComponentTextoInformacoes: React.FC<TextoInformacoesProps> = ({
     titulo,
     previa,
-    autor,
+    autor, //para quem  for fazer a integração, o molde do autor vai ser Dr.Nome (CRM/UF 123456)
     data,
     texto,
     referencias,
@@ -21,16 +21,16 @@ const ComponentTextoInformacoes: React.FC<TextoInformacoesProps> = ({
     return (
         <div className="texto-informacoes-box">
             <div className="texto-informacoes-titulo">{titulo}</div>
-            
+
             <div className="texto-informacoes-previa">{previa}</div>
-            <br/>
+            <br />
             <div className="texto-informacoes-data-autor">{data},{autor}</div>
-            <br/>
+            <br />
             <div className="texto-informacoes-texto">{texto}</div>
-            <br/>
+            <br />
             <div className="texto-titulo-referencia">Referências</div>
-            <br/>
-            <div className="texto-informacoes-referencias">{referencias}</div>
+            <br />
+            <div className="texto-informacoes-referencias" >{referencias.map(x => <a key={x} href={x}> {x} </a>)}</div>
         </div>
     );
 };
