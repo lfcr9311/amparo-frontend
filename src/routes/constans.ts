97fe4c4f6094295221430a92d474c3f16803cfdd
Luis Felipe Copetti Rossi <rossi.luis@edu.pucrs.br>
2023-11-08 17:30:37 -0300

    iniciando repo

--- src/routes/constans.ts
+++ src/routes/constans.ts
@@ -16,4 +16,5 @@ export const ROUTES = {
   EDICAO_EXAMES_REALIZADOS: () => '/exames/edicao/realizados',
   LISTADEINTERACAODOMEDICAMENTO: () => '/medicamentos/interacao',
   FILTROBUSCAMEDICO: () => '/lista/medico',
+  EXAMESPENDENTESMEDICO: () => '/exames/pendentes/medico',
 };
