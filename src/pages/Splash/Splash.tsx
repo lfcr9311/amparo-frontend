36d06b438dd20ee9e81917cc1eff7bd15196eb5c
Pablo Montiel <pablopmontiel98@gmail.com>
2023-11-11 16:57:22 -0300

    Splash screen pronta

--- src/pages/Splash/Splash.tsx
+++ src/pages/Splash/Splash.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Lottie from 'lottie-web';
 import animationData from '../../assets/Splash.json';
-import './Splash.css';
 
 class Splash extends Component {
     animationContainerRef = React.createRef<HTMLDivElement>();
@@ -21,12 +20,11 @@ class Splash extends Component {
     render() {
         return (
             <div
-                className='splash-size'
                 ref={this.animationContainerRef}
                 style={{
-                    width: '100%',
-                    height: '100%',
-                    backgroundColor: '#fbf8f8',
+                    width: '60%',
+                    height: '60%',
+                    margin: 'auto'
                 }}
             ></div>
         );
