bc8fcdf15c71d6a8e18d2b89112cb47243ace63b
Leonardo Mello <leonardo.mello@totvs.com.br>
2023-11-10 21:28:56 -0300

    escrevendo no buscar

--- src/pages/BuscaMedicamentos/BuscaMedicamentos.tsx
+++ src/pages/BuscaMedicamentos/BuscaMedicamentos.tsx
@@ -16,18 +16,23 @@ interface Medicamento {
 
 export default function BuscaMedicamentos() {
     const [findMedicine, setMedicine] = useState("");
-    
+
+    const handleMed = (newValue: string) => {
+        setMedicine(newValue);
+        console.log(newValue);
+    }
+
     const medicamentos: Medicamento[] = [
         { label: "Remedio 1" }
     ];
     return (
         <>
             <HeaderHome title="Medicamentos" type="headerPage" />
-           <p className='busca-medicamento-title'>Remedios</p>
+            <p className='busca-medicamento-title'>Remedios</p>
             <div className='search-div-medicamentos'>
                 <FiltroBusca
                     value={findMedicine}
-                    onChange={(e) => setMedicine(e.target.value)}
+                    onChange={handleMed}
                 />
             </div>
             <div className='filter-div-medicamentos'>
@@ -40,13 +45,13 @@ export default function BuscaMedicamentos() {
                 />
             </div>
             <div className="remedios-div">
-            {medicamentos.map((medicamento, index) => (
-                <CardMedicine
-                    key={index}
-                    exam={medicamento.label}
-                    onChange={() => console.log(medicamento.label)}
-                />
-            ))}
+                {medicamentos.map((medicamento, index) => (
+                    <CardMedicine
+                        key={index}
+                        exam={medicamento.label}
+                        onChange={() => console.log(medicamento.label)}
+                    />
+                ))}
             </div>
             <Footer user="patient" />
         </>
