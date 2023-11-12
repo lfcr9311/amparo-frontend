958bc627a797ccb7846f4701cad81e1d4304e3f4
Pablo Montiel <pablopmontiel98@gmail.com>
2023-11-12 13:58:24 -0300

    carregamento e exemplo no MenuMedicamento.tsx

--- src/pages/MenuMedicamentos/MenuMedicamentos.tsx
+++ src/pages/MenuMedicamentos/MenuMedicamentos.tsx
@@ -28,3 +28,55 @@ export default function MenuMedicamentos() {
     </>
   );
 }
+
+
+
+
+/*
+
+import { useState, useEffect } from 'react';
+import { CardMedicamentos } from '../../components/CardMedicamentos/cardMedicamentos';
+import Footer from '../../components/Footer/Footer';
+import HeaderHome from '../../components/HeaderHome/HeaderHome';
+import './MenuMedicamentos.css';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../../routes/constans';
+import LoadingOverlay from '../../components/Loading/Overlay';
+
+export default function MenuMedicamentos() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  }, []);
+
+  const handleClickMyList = () => {
+    navigate(ROUTES.LISTA_MEDICAMENTOS());
+  };
+
+  return (
+    <>
+      <HeaderHome title="Medicamentos" type="headerPage" />
+      {isLoading ? (
+        <LoadingOverlay />
+      ) : (
+        <div className="menu-medicamentos-container">
+          <CardMedicamentos iconType="icon1" onClick={handleClickMyList} />
+          <CardMedicamentos
+            iconType="icon2"
+            onClick={() => {
+              console.log('agenda remedios');
+            }}
+          />
+        </div>
+      )}
+      <Footer user="patient" />
+    </>
+  );
+}
+
+
+*/
