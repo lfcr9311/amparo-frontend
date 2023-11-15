1a6ead6e3e39fe3eba880611259ac17e9dbed268
João Coelho <joaoantonioscoelho@gmail.com>
2023-11-15 16:40:40 -0300

    integracao perfil medico

--- src/components/ProfileDoctorCard/ProfileDoctorCard.tsx
+++ src/components/ProfileDoctorCard/ProfileDoctorCard.tsx
@@ -7,71 +7,77 @@ import {Switch} from '@mui/material';
 import {useState} from 'react';
 
 interface ProfileDoctorCardProps {
-    name: string;
-    crm?: string;
-    profilePicture?: string;
-    email?: string;
-    phone?: string;
-    edit: () => void;
-    alterarSenha: () => void;
-    uf?: string;
+  name: string;
+  crm?: string;
+  profilePicture?: string;
+  email?: string;
+  phone?: string;
+  edit: () => void;
+  alterarSenha: () => void;
+  uf?: string;
 }
 
 export default function ProfileDoctorCard({
-                                              name, uf, crm, email, phone, edit, alterarSenha, profilePicture,
+                                            name, uf, crm, email, phone, edit, alterarSenha, profilePicture,
                                           }: ProfileDoctorCardProps) {
-    const [notifications, setNotifications] = useState(false);
+  const [notifications, setNotifications] = useState(false);
 
-    const changeNotifications = (notifications: boolean) => {
-        if (notifications) console.log('Notificações desabilitadas'); else console.log('Notificações habilitadas');
-    };
+  const changeNotifications = (notifications: boolean) => {
+    if (notifications) console.log('Notificações desabilitadas'); else console.log('Notificações habilitadas');
+  };
 
-    return (<div className="card-container">
-            <button onClick={edit} className="edit-button">
-                <img src={EditIcon}/>
-            </button>
-            <img
-                src={profilePicture ? profilePicture : IconProfile}
-                className="profile-icon"
-            />
-            <p className="name">{name}</p>
-            <div className="text-box">
-                <div>
-                    <label className="label">CRM / UF:</label>
-                    <p className="agreements">{`${crm}/${uf}`}</p>
-                </div>
-                <div className="notifications-container">
-                    <p className="notifications">Habilitar Notificacoes</p>
-                    <Switch
-                        sx={{marginLeft: '80px', marginTop: '5px'}}
-                        checked={notifications}
-                        onChange={() => {
-                            setNotifications(!notifications);
-                            changeNotifications(notifications);
-                        }}
-                    />
-                </div>
-            </div>
-            <div className="aux-box">
+  return (<div className="card-container">
+    <button onClick={edit} className="edit-button">
+      <img src={EditIcon}/>
+    </button>
+    <img
+      src={profilePicture ? profilePicture : IconProfile}
+      className="profile-icon"
+    />
+    <p className="name">{name}</p>
+    <p className="specialty">Infectologista</p>
+    <div className="text-box">
+      <div>
+        <label className="label">Convênios:</label>
+        <p className="agreements">Unimed SulaAmerica</p>
+      </div>
+
+      <div>
+        <label className="label">CRM / UF:</label>
+        <p className="agreements">{`${crm}/${uf}`}</p>
+      </div>
+      <div className="notifications-container">
+        <p className="notifications">Habilitar Notificacoes</p>
+        <Switch
+          sx={{marginLeft: '80px', marginTop: '5px'}}
+          checked={notifications}
+          onChange={() => {
+            setNotifications(!notifications);
+            changeNotifications(notifications);
+          }}
+        />
+      </div>
+    </div>
+    <div className="aux-box">
         <span className="itens">
           <img
-              src={EmailIcon}
-              className="aux-box-icon email-icon"
-              alt="Email"
+            src={EmailIcon}
+            className="aux-box-icon email-icon"
+            alt="Email"
           />
-            {email}
+          {email}
         </span>
-                <span className="itens">
+      <span className="itens">
           <img
-              src={PhoneIcon}
-              className="aux-box-icon phone-icon"
-              alt="Phone"
+            src={PhoneIcon}
+            className="aux-box-icon phone-icon"
+            alt="Phone"
           />
-                    {phone}
+        {phone}
         </span>
-            </div>
-            <button className="alterar-senha-button" onClick={alterarSenha}>
-                Alterar senha
-            </button>
-        </div>);
+    </div>
+    <button className="alterar-senha-button" onClick={alterarSenha}>
+      Alterar senha
+    </button>
+  </div>);
 }
--- src/pages/VisualizarPerfilMedico/VisualizarPerfilMedico.tsx
+++ src/pages/VisualizarPerfilMedico/VisualizarPerfilMedico.tsx
@@ -10,194 +10,219 @@ import CustomButton from '../../components/Button/Button';
 import {Alert, Button, CircularProgress, Snackbar} from '@mui/material';
 import {useNavigate} from 'react-router-dom';
 import {ROUTES} from '../../routes/constans';
-import {getDoctor} from "../../utils/apiService.tsx";
-import api from "../../api.tsx";
+import {getDoctor, updateDoctor} from "../../utils/apiService.tsx";
 
 interface Doctor {
-    cellphone?: string;
-    crm?: string;
-    email?: string;
-    id?: string;
-    isAnonymous?: boolean;
-    name?: string;
-    profilePicture?: string;
-    uf?: string;
+  cellphone: string;
+  crm?: string;
+  email?: string;
+  id?: string;
+  isAnonymous?: boolean;
+  name?: string;
+  profilePicture?: string;
+  uf?: string;
 }
 
 const VisualizacaoPerfilMedico = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [doctor, setDoctor] = useState<Doctor>();
-    const [name, setName] = useState('');
-    const [uf, setUf] = useState('');
-    const [crm, setCrm] = useState('');
-    const [ddd, setDdd] = useState('');
-    const [phone, setPhone] = useState('');
-    const [isLoading, setIsLoading] = useState(true);
-    const [successSnackbar, setSuccessSnackbar] = useState(false);
-    const [errorSnackbar, setErrorSnackbar] = useState(false);
-    const navigate = useNavigate()
-    
-    const handleDeletar = () => {
-        console.log("im here");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [doctor, setDoctor] = useState<Doctor>();
+  const [name, setName] = useState('');
+  const [uf, setUf] = useState('');
+  const [crm, setCrm] = useState('');
+  const [ddd, setDdd] = useState('');
+  const [phone, setPhone] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [successSnackbar, setSuccessSnackbar] = useState(false);
+  const [errorSnackbar, setErrorSnackbar] = useState(false);
+  const navigate = useNavigate()
 
-        localStorage.removeItem('authToken')
-        localStorage.removeItem('userId')
-        navigate(ROUTES.LOGIN())
+  const handleDeletar = () => {
+    console.log("im here");
+
+    localStorage.removeItem('authToken')
+    localStorage.removeItem('userId')
+    navigate(ROUTES.LOGIN())
+  }
+
+  const resetFields = () => {
+    setName('');
+    setUf('');
+    setCrm('');
+    setDdd('');
+    setPhone('');
+  }
+
+  useEffect(() => {
+    setIsLoading(true)
+    getDoctor().then((response) => {
+      setDoctor(response.data)
+      setIsLoading(false)
+
+    }).catch((error) => {
+      console.log(error)
+      setIsLoading(false)
+
+    })
+  }, []);
+
+  useEffect(() => {
+    if (setSuccessSnackbar) {
+      setTimeout(() => {
+        setSuccessSnackbar(false);
+      }, 2000);
     }
+  }, [successSnackbar]);
 
-    useEffect(() => {
-        setIsLoading(true)
-        getDoctor().then((response) => {
-            setDoctor(response.data)
-            setIsLoading(false)
+  useEffect(() => {
+    if (errorSnackbar) {
+      setTimeout(() => {
+        setErrorSnackbar(false);
+      }, 2000);
+    }
+  }, [errorSnackbar]);
 
-        }).catch((error) => {
-            console.log(error)
-            setIsLoading(false)
+  // @ts-ignore
+  return (<>
+    <div className="header-container">
+      <HeaderHome title="Perfil" type="headerPage"/>
+    </div>
+    {isLoading && <CircularProgress color='error'/>}
+    {!isLoading && (<>
 
-        })
-    }, []);
-
-    useEffect(() => {
-        if (setSuccessSnackbar) {
-            setTimeout(() => {
-                setSuccessSnackbar(false);
-            }, 2000);
-        }
-    }, [successSnackbar]);
-
-    useEffect(() => {
-        if (errorSnackbar) {
-            setTimeout(() => {
-                setErrorSnackbar(false);
-            }, 2000);
-        }
-    }, [errorSnackbar]);
-
-    // @ts-ignore
-    return (<>
-        <div className="header-container">
-            <HeaderHome title="Perfil" type="headerPage"/>
+      <div className="container">
+        <div className="profile-card-container">
+          <ProfileDoctorCard
+            name={`Dr. ${doctor?.name}`}
+            uf={doctor?.uf}
+            crm={doctor?.crm}
+            email={doctor?.email}
+            phone={doctor?.cellphone?.replace(/^(\d{2})(\d+)$/, '($1) $2')}
+            edit={() => setIsModalOpen(!isModalOpen)}
+            alterarSenha={() => console.log('Alterar Senha')}
+          />
         </div>
-        {isLoading && <CircularProgress color='error'/>}
-        {!isLoading && (<>
+        <Modal
+          isOpen={isModalOpen}
+          title="Pefil"
+          isClose={() => {
+            setIsModalOpen(!isModalOpen);
+            setName('');
+            setUf('');
+            setCrm('');
+            setDdd('');
+            setPhone('');
+          }}
+        >
+          <form>
+            <div className="content-modal">
+              <TextfieldModal
+                label="Seu nome"
+                value={name}
+                type="text"
+                onChange={(value) => setName(value)}
+              />
+              <div className="container-aux">
+                <SelectModal onChange={(value) => setUf(value)} value={uf}/>
+                <TextfieldModal
+                  label="CRM"
+                  value={crm}
+                  type="text"
+                  onChange={(value) => setCrm(value)}
+                  width="195.5px"
+                />
+              </div>
+              <div className="container-aux">
+                <TextfieldModal
+                  label="DDD"
+                  value={ddd}
+                  type="text"
+                  onChange={(value) => setDdd(value)}
+                  width="65.5px"
+                />
+                <TextfieldModal
+                  label="Telefone"
+                  value={phone}
+                  type="text"
+                  onChange={(value) => setPhone(value)}
+                  width="195.5px"
+                />
+              </div>
+              {/*<SelectConvenios*/}
+              {/*    onChange={(value) => setConvenios(value)}*/}
+              {/*    value={convenios}*/}
+              {/*/>*/}
+              <CustomButton
+                variant="contained"
+                label="Salvar"
+                onClick={() => {
+                  console.log('ddd: ', ddd);
+                  let newPhone: string | undefined = '';
 
-            <div className="container">
-                <div className="profile-card-container">
-                    <ProfileDoctorCard
-                        name={`Dr. ${doctor?.name}`}
-                        uf={doctor?.uf}
-                        crm={doctor?.crm}
-                        email={doctor?.email}
-                        phone={doctor?.cellphone?.replace(/^(\d{2})(\d+)$/, '($1) $2')}
-                        edit={() => setIsModalOpen(!isModalOpen)}
-                        alterarSenha={() => console.log('Alterar Senha')}
-                    />
-                </div>
-                <Modal
-                    isOpen={isModalOpen}
-                    title="Pefil"
-                    isClose={() => setIsModalOpen(!isModalOpen)}
-                >
-                    <form>
-                        <div className="content-modal">
-                            <TextfieldModal
-                                label="Seu nome"
-                                value={name}
-                                type="text"
-                                onChange={(value) => setName(value)}
-                            />
-                            <div className="container-aux">
-                                <SelectModal onChange={(value) => setUf(value)} value={uf}/>
-                                <TextfieldModal
-                                    label="CRM"
-                                    value={crm}
-                                    type="text"
-                                    onChange={(value) => setCrm(value)}
-                                    width="195.5px"
-                                />
-                            </div>
-                            <div className="container-aux">
-                                <TextfieldModal
-                                    label="DDD"
-                                    value={ddd}
-                                    type="text"
-                                    onChange={(value) => setDdd(value)}
-                                    width="65.5px"
-                                />
-                                <TextfieldModal
-                                    label="Telefone"
-                                    value={phone}
-                                    type="text"
-                                    onChange={(value) => setPhone(value)}
-                                    width="195.5px"
-                                />
-                            </div>
-                            {/*<SelectConvenios*/}
-                            {/*    onChange={(value) => setConvenios(value)}*/}
-                            {/*    value={convenios}*/}
-                            {/*/>*/}
-                            <CustomButton
-                                variant="contained"
-                                label="Salvar"
-                                onClick={() => {
-                                    const newPhone = ddd + phone;
-                                    api.put('/doctor', {
-                                        // send only the fields that were changed
-                                        name: name !== '' ? name : undefined, uf: uf !== '' ? uf : undefined, crm: crm !== '' ? crm : undefined, cellphone: newPhone !== '' ? newPhone : undefined,
-                                    }, {
-                                        headers: {
-                                            'Authorization': 'Bearer ' + localStorage.getItem("authToken")
-                                        }
-                                    }).then((response) => {
-                                        console.log(response)
-                                        setName('')
-                                        setUf('')
-                                        setCrm('')
-                                        setDdd('')
-                                        setPhone('')
-                                        setIsModalOpen(!isModalOpen)
-                                        setSuccessSnackbar(true)
-                                    }).catch((error) => {
-                                        setErrorSnackbar(true)
-                                        console.log(error)
-                                    })
-                                }}
-                            />
-                        </div>
-                    </form>
-                </Modal>
-                <div>
-                    <Button
-                        sx={{
-                            color: '#e10e17', textAlign: 'center', fontFamily: 'Poppins', fontSize: '15px', fontStyle: 'normal', fontWeight: 400, lineHeight: 'normal', textDecoration: 'none', textTransform: 'none',
-                        }}
-                        onClick={handleDeletar}
-                    >
-                        Sair da Conta
-                    </Button>
-                </div>
-            </div>
-            <Snackbar open={successSnackbar} autoHideDuration={6000}>
-                <Alert onClose={() => {
-                    setSuccessSnackbar(false);
-                }} severity="success" sx={{ width: '100%' }}>
-                    Informações editadas com sucesso!
-                </Alert>
-            </Snackbar>
+                  if (ddd !== '' && phone !== '') {
+                    newPhone = ddd + phone;
+                  } else if (ddd === '' && phone !== '') {
+                    newPhone = doctor?.cellphone?.substring(0, 2) + phone;
+                  } else if (ddd !== '' && phone === '') {
+                    newPhone = ddd + doctor?.cellphone;
+                  } else {
+                    newPhone = doctor?.cellphone;
+                  }
 
-            <Snackbar open={errorSnackbar} autoHideDuration={6000}>
-                <Alert onClose={() => {
-                    setErrorSnackbar(false);
-                }} severity="error" sx={{ width: '100%' }}>
-                    Erro ao editar informações!
-                </Alert>
-            </Snackbar>
-            <div className="footer-container">
-                <Footer user="doctor"/>
+                  updateDoctor(name, uf, crm, newPhone, doctor).then(() => {
+                    resetFields();
+                    setIsModalOpen(!isModalOpen);
+                    setSuccessSnackbar(true);
+                    window.location.reload();
+                  }).catch((err) => {
+                    resetFields();
+                    setErrorSnackbar(true);
+                    console.error(err);
+                    window.location.reload();
+                  })
+                }}
+              />
             </div>
-        </>)}
-    </>);
+          </form>
+        </Modal>
+        <div>
+          <Button
+            sx={{
+              color: '#e10e17',
+              textAlign: 'center',
+              fontFamily: 'Poppins',
+              fontSize: '15px',
+              fontStyle: 'normal',
+              fontWeight: 400,
+              lineHeight: 'normal',
+              textDecoration: 'none',
+              textTransform: 'none',
+            }}
+            onClick={handleDeletar}
+          >
+            Sair da Conta
+          </Button>
+        </div>
+      </div>
+      <Snackbar open={successSnackbar} autoHideDuration={6000}>
+        <Alert onClose={() => {
+          setSuccessSnackbar(false);
+        }} severity="success" sx={{width: '100%'}}>
+          Informações editadas com sucesso!
+        </Alert>
+      </Snackbar>
+
+      <Snackbar open={errorSnackbar} autoHideDuration={6000}>
+        <Alert onClose={() => {
+          setErrorSnackbar(false);
+        }} severity="error" sx={{width: '100%'}}>
+          Erro ao editar informações!
+        </Alert>
+      </Snackbar>
+      <div className="footer-container">
+        <Footer user="doctor"/>
+      </div>
+    </>)}
+  </>);
 };
 
 export default VisualizacaoPerfilMedico;
